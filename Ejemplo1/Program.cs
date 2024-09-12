86c141d9e28d4b6a1d496140dec81dcffb4fcedb
Juan José Blanco Días <bladiaju@gregoriofer.local>
2024-09-12 12:59:25 +0200

    Hola

---
+++ Ejemplo1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+    Ejemplo 1
+*/
+Console.WriteLine("Hola Tete, cómo te llamas?");
+Console.WriteLine("Escribe tu nombre: ");
+String? nombre = Console.ReadLine();
+if (nombre == String.Empty)
+    Console.WriteLine("Es nulo");
+else
+    //Console.WriteLine("Hola " + nombre);
+    Console.WriteLine($"Hola {nombre.ToUpper()}");
