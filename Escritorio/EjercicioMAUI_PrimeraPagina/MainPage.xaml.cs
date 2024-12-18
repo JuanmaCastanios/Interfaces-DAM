2a2eaf69900bc8893473012138ca68493fc7f3c9
JuanmaCastanios <juanjoseblancodiaz1504@gmail.com>
2024-12-18 10:35:40 +0100

    Ejercicio Mvvm

---
+++ Escritorio/EjercicioMAUI_PrimeraPagina/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace EjercicioMAUI_PrimeraPagina
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ Escritorio/EjercicioMAUI_PrimeraPagina/Restaurante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace EjercicioMAUI_PrimeraPagina
+{
+    internal class Restaurante
+    {
+        public string Titulo { get; set; }
+        public string Nombre { get; set; }
+        public string Lema { get; set; }
+        public string Direccion { get; set; }
+        public string Localidad { get; set; }
+        public string Provincia { get; set; }
+        public string Foto { get; set; }
+        public Color ColorBase { get; set; }
+    }
+}
